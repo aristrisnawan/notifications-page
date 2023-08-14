4cb688b88811902cc2859c71ea667f6fc717be9e
aristrisnawan <aristrisnawan35@gmail.com>
2023-08-14 15:09:44 +0700

    create card message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        tablet: "640px",
+        laptop: "1024px",
+        desktop: "1280px",
+      },
+    },
   },
   plugins: [],
 }
